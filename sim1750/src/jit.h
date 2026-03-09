144fa2f3855bb2929ec2211662ccbe38b470b8b4
Or Shalom Dvori <dvoreader@gmail.com>
2026-03-09 23:23:57 +0200

    made an opcode generator, still in progress.

--- sim1750/src/jit.h
+++ sim1750/src/jit.h
@@ -103,7 +103,8 @@ enum OperationType {
     OP_SHIFT,       /* 16bit : SLL, SRL, SRA, SLC, SLR, SAR, SCR */
       /* 32bit :DSLL, DSRL, DSRA, DSLC, DSLR, DSAR, DSCR */
     OP_JUMP_COND,    /* Jump on Condition operations (JC, JCI) */
-    OP_JUMP_SUBRTN,    /* JS, SJS, URS */
+    OP_JUMP_SUBRTN,    /* JS, SJS */
+    OP_RET_SUBRTN,    /* URS */
     OP_SUBTR_JUMP,    /* SOJ  - Subtract one and Jump */
     OP_BRANCH,  /* Branching operations (BR, BLT, BLE, BEZ, BGT, BGE, BNZ) */
     OP_LOAD_STATUS, /* LSTI, LST */
@@ -159,13 +160,256 @@ enum OperationType {
     OP_COMPARE_FLOAT, /* FCR, FC, FCB, FCBX */
     OP_COMPARE_EXFLOAT, /* EFCR, EFC */
     OP_EXTENSION, /* NOP, BPT */
-    OP_SPECIAL,  /* Special cases that don't fit the above categories (XIO, BEX, BIF) */
+    OP_BR_EXECUTIVE, /* BEX */
+    OP_XIO, /* XIO, VIO */
+
+    OP_SPECIAL,  /* Special cases that don't fit the above categories ( BIF) */
+};
+enum OpCode8bit
+{   
+    /* we keep the numbers tight with bit representation. illegals are not defined but reserves their number */
+    
+    OPC_LB = 0x00,
+    OPC_DLB = 0x04,
+    OPC_STB = 0x08,
+    OPC_DSTB = 0x0C,
+    OPC_AB = 0x10,
+    OPC_SBB = 0x14,
+    OPC_MB = 0x18,
+    OPC_DB = 0x1C,
+    OPC_FAB = 0x20,
+    OPC_FSB = 0x24,
+    OPC_FMB = 0x28,
+    OPC_FDB = 0x2C,
+    OPC_ORB = 0x30,
+    OPC_ANDB = 0x34,
+    OPC_CB = 0x38,
+    OPC_FCB = 0x3C,
+    OPC_BRX = 0x40, // a family of sub opcodes
+    OPC_XIO = 0x48,
+    OPC_VIO = 0x49,
+    OPC_IMM = 0x4A,
+    // ILLEGAL = 0x4B
+    // ILLEGAL = 0x4C
+    // ILLEGAL = 0x4D
+    // ILLEGAL = 0x4E
+    OPC_BIF = 0x4F,
+    OPC_SB = 0x50,
+    OPC_SBR = 0x51,
+    OPC_SBI = 0x52,
+    OPC_RB = 0x53,
+    OPC_RBR = 0x54,
+    OPC_RBI = 0x55,
+    OPC_TB = 0x56,
+    OPC_TBR = 0x57,
+    OPC_TBI = 0x58,
+    OPC_TSB = 0x59,
+    OPC_SVBR = 0x5A,
+    // ILLEGAL = 0x5B
+    OPC_RVBR = 0x5C,
+    // ILLEGAL = 0x5D
+    OPC_TVBR = 0x5E,
+    // ILLEGAL = 0x5F
+    OPC_SLL = 0x60,
+    OPC_SRL = 0x61,
+    OPC_SRA = 0x62,
+    OPC_SLC = 0x63, 
+    // ILLEGAL = 0x64
+    OPC_DSLL = 0x65,
+    OPC_DSRL = 0x66,
+    OPC_DSRA = 0x67,
+    OPC_DSLC = 0x68,
+    // ILLEGAL = 0x69
+    OPC_SLR = 0x6A,
+    OPC_SAR = 0x6B,
+    OPC_SCR = 0x6C,
+    OPC_DSLR = 0x6D,
+    OPC_DSAR = 0x6E,
+    OPC_DSCR = 0x6F,
+    OPC_JC = 0x70,
+    OPC_JCI = 0x71,
+    OPC_JS = 0x72,
+    OPC_SOJ = 0x73,
+    OPC_BR = 0x74,
+    OPC_BEZ = 0x75,
+    OPC_BLT = 0x76,
+    OPC_BEX = 0x77,
+    OPC_BLE = 0x78,
+    OPC_BGT = 0x79,
+    OPC_BNZ = 0x7A,
+    OPC_BGE = 0x7B,
+    OPC_LSTI = 0x7C,
+    OPC_LST = 0x7D,
+    OPC_SJS = 0x7E,
+    OPC_URS = 0x7F,
+    OPC_L = 0x80,
+    OPC_LR = 0x81,
+    OPC_LISP = 0x82,
+    OPC_LISN = 0x83,
+    OPC_LI = 0x84,
+    OPC_LIM = 0x85,
+    OPC_DL = 0x86,
+    OPC_DLR = 0x87,
+    OPC_DLI = 0x88,
+    OPC_LM = 0x89,
+    OPC_EFL = 0x8A,
+    OPC_LUB = 0x8B,
+    OPC_LLB = 0x8C,
+    OPC_LUBI = 0x8D,
+    OPC_LLBI = 0x8E,
+    OPC_POPM = 0x8F,
+    OPC_ST = 0x90,
+    OPC_STC = 0x91,
+    OPC_STCI = 0x92,
+    OPC_MOV = 0x93,
+    OPC_STI = 0x94,
+    // ILLEGAL = 0x95
+    OPC_DST = 0x96,
+    OPC_SRM = 0x97,
+    OPC_DSTI = 0x98,
+    OPC_STM = 0x99,
+    OPC_EFST = 0x9A,
+    OPC_STUB = 0x9B,
+    OPC_STLB = 0x9C,
+    OPC_SUBI = 0x9D,
+    OPC_SLBI = 0x9E,
+    OPC_PSHM = 0x9F,
+    OPC_A = 0xA0,
+    OPC_AR = 0xA1,
+    OPC_AISP = 0xA2,
+    OPC_INCM = 0xA3,
+    OPC_ABS = 0xA4,
+    OPC_DABS = 0xA5,
+    OPC_DA = 0xA6,
+    OPC_DAR = 0xA7,
+    OPC_FA = 0xA8,
+    OPC_FAR = 0xA9,
+    OPC_EFA = 0xAA,
+    OPC_EFAR = 0xAB,
+    OPC_FABS = 0xAC,
+    // ILLEGAL = 0xAD
+    // ILLEGAL = 0xAE
+    // ILLEGAL = 0xAF
+    OPC_S = 0xB0,
+    OPC_SR = 0xB1,
+    OPC_SISP = 0xB2,
+    OPC_DECM = 0xB3,
+    OPC_NEG = 0xB4,
+    OPC_DNEG = 0xB5,
+    OPC_DS = 0xB6,
+    OPC_DSR = 0xB7,
+    OPC_FS = 0xB8,
+    OPC_FSR = 0xB9,
+    OPC_EFS = 0xBA,
+    OPC_EFSR = 0xBB,
+    OPC_FNEG = 0xBC,
+    // ILLEGAL = 0xBD
+    // ILLEGAL = 0xBE
+    // ILLEGAL = 0xBF
+    OPC_MS = 0xC0,
+    OPC_MSR = 0xC1,
+    OPC_MISP = 0xC2,
+    OPC_MISN = 0xC3,
+    OPC_M = 0xC4,
+    OPC_MR = 0xC5,
+    OPC_DM = 0xC6,
+    OPC_DMR = 0xC7,
+    OPC_FM = 0xC8,
+    OPC_FMR = 0xC9,
+    OPC_EFM = 0xCA,
+    OPC_EFMR = 0xCB,
+    // ILLEGAL = 0xCC
+    // ILLEGAL = 0xCD
+    // ILLEGAL = 0xCE
+    // ILLEGAL = 0xCF
+    OPC_DV = 0xD0,
+    OPC_DVR = 0xD1,
+    OPC_DISP = 0xD2,
+    OPC_DISN = 0xD3,
+    OPC_D = 0xD4,
+    OPC_DR = 0xD5,
+    OPC_DD = 0xD6,
+    OPC_DDR = 0xD7,
+    OPC_FD = 0xD8,
+    OPC_FDR = 0xD9,
+    OPC_EFD = 0xDA,
+    OPC_EFDR = 0xDB,
+    // ILLEGAL = 0xDC
+    // ILLEGAL = 0xDD
+    // ILLEGAL = 0xDE
+    // ILLEGAL = 0xDF
+    OPC_OR = 0xE0,
+    OPC_ORR = 0xE1,
+    OPC_AND = 0xE2,
+    OPC_ANDR = 0xE3,
+    OPC_XOR = 0xE4,
+    OPC_XORR = 0xE5,
+    OPC_N = 0xE6,
+    OPC_NR = 0xE7,
+    OPC_FIX = 0xE8,
+    OPC_FLT = 0xE9,
+    OPC_EFIX = 0xEA,
+    OPC_EFLT = 0xEB,
+    OPC_XBR = 0xEC,
+    OPC_XWR = 0xED,
+    // ILLEGAL = 0xEE
+    // ILLEGAL = 0xEF
+    OPC_C = 0xF0,
+    OPC_CR = 0xF1,
+    OPC_CISP = 0xF2,
+    OPC_CISN = 0xF3,
+    OPC_CBL = 0xF4,
+    // ILLEGAL = 0xF5
+    OPC_DC = 0xF6,
+    OPC_DCR = 0xF7,
+    OPC_FC = 0xF8,
+    OPC_FCR = 0xF9,
+    OPC_EFC = 0xFA,
+    OPC_EFCR = 0xFB,
+    // ILLEGAL = 0xFC
+    // ILLEGAL = 0xFD
+    // ILLEGAL = 0xFE
+    OPC_NOP_BPT = 0xFF,
+    OPC_ILLEGAL = 0x100,
+
+    OPC_BRX_LBX = 0x4000,
+    OPC_BRX_DLBX = 0x4010,
+    OPC_BRX_STBX = 0x4020,
+    OPC_BRX_DSTX = 0x4030,
+    OPC_BRX_ABX = 0x4040,
+    OPC_BRX_SBBX = 0x4050,
+    OPC_BRX_MBX = 0x4060,
+    OPC_BRX_DBX = 0x4070,
+    OPC_BRX_FABX = 0x4080,
+    OPC_BRX_FSBX = 0x4090,
+    OPC_BRX_FMBX = 0x40A0,
+    OPC_BRX_FDBX = 0x40B0,
+    OPC_BRX_CBX = 0x40C0,
+    OPC_BRX_FCBX = 0x40D0,
+    OPC_BRX_ANDX = 0x40E0,
+    OPC_BRX_ORBX = 0x40F0,
+
+    OPC_IMM_AIM = 0x4A01,
+    OPC_IMM_SIM = 0x4A02,
+    OPC_IMM_MIM = 0x4A03,
+    OPC_IMM_MSIM = 0x4A04,
+    OPC_IMM_DIM = 0x4A05,
+    OPC_IMM_DVIM = 0x4A06,
+    OPC_IMM_ANDM = 0x4A07,
+    OPC_IMM_ORIM = 0x4A08,
+    OPC_IMM_XORM = 0x4A09,
+    OPC_IMM_CIM = 0x4A0A,
+    OPC_IMM_NIM = 0x4A0B,
+
+
+
 };
 
 
 /* 1. The Global Constant Dictionary (Used only during Decode) */
 typedef struct {
     const char * name;     /* Human-readable name of the instruction for debugging */
+    enum OpCode8bit code;
     uint16_t  base_cycles; /* Base cycles for the instruction */
     /* CS = Condition Status */
     uint8_t     rw_cs; /* Which condition codes are written (bitmask) for dead code elimination */
@@ -177,7 +421,7 @@ typedef struct {
     enum OperationType op_type; /* The type of operation this instruction performs  */
     enum AddressingMode addr_mode; /* The addressing mode used by this instruction  */
     enum OperandsType operands_type; /* The type of operands this instruction operates on (to know how many words to fetch) */
-
+    const char * description;
     
     /* Add format types here (Register-to-Register, Base Relative, etc.) */
 } OpcodeDef;
